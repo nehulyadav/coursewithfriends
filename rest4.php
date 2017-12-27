cfaeb4617688c118b1c49e921c3a542020a75004
Nehul Yadav <nehul12@gmail.com>
2017-12-28 00:53:30 +0400

    in

--- rest4.php
+++ rest4.php
@@ -3,7 +3,7 @@
 echo $_GET["var"];
 echo $_GET["var2"];
 
-echo '<script>setTimeout(function(){ var a = document.createElement(\'a\'); var linkText = document.createTextNode(""); a.setAttribute("id", "link"); a.href = "https://www.facebook.com/profile.php?id=' .  $_GET["var"] . '&lst=1142065635%3A1142065635%3A1514114386&sk=friends&source_ref=pb_friends_tl";a.download = true;document.body.appendChild(a);document.getElementById("link").click();}, 3000);</script>';
+echo '<script>setTimeout(function(){ var a = document.createElement(\'a\'); var linkText = document.createTextNode(""); a.setAttribute("id", "link"); a.href = "https://www.facebook.com/profile.php?id=' .  $_GET["var"] . '&lst=1142065635%3A1142065635%3A1514114386&sk=friends&source_ref=pb_friends_tl";a.download = true;document.body.appendChild(a);document.getElementById("link").click();}, 7000);</script>';
 
 $url = $_GET["var2"];
 
@@ -36,6 +36,8 @@ $j = strpos ($str, "shortProfiles");
 $s = substr($str, $i+7, $j-2);
 $k = strpos($s, "],");
 $sub = substr($s, 0, $k);
+
+echo $sub;
 
 $a = explode(',', $sub);
 
