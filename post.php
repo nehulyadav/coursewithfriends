caf78f4affeb41035e4d3e9f75ffbd6f6e364279
Nehul Yadav <nehul12@gmail.com>
2017-12-29 17:17:11 +0400

    in

--- post.php
+++ post.php
@@ -1,29 +1,12 @@
 <?php
-//extract data from the post
-extract($_POST);
+$s = "f";
 
-//set POST variables
-$url = 'https://xvxssuxdle.localtunnel.me/rest4.php';
-$fields = array(
-            'var' => "annonymous",
-            'var2' => "a",
-        );
+echo '
+<script>
 
-//url-ify the data for the POST
-$field_string = http_build_query($fields);
+    location.replace("https://xvxssuxdle.localtunnel.me/rest4.php?var=" . $s);
 
-//open connection
-$ch = curl_init();
+</script>
 
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL, $url);
-curl_setopt($ch,CURLOPT_POST, 1);
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-//execute post
-$result = curl_exec($ch);
-echo $result;
-
-//close connection
-curl_close($ch);
+';
 ?>
