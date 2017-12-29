968fe4c29f3f32b0c1b32da21c8fcb365002b64b
Nehul Yadav <nehul12@gmail.com>
2017-12-29 17:46:10 +0400

    in

--- post.php
+++ post.php
@@ -1,12 +1,7 @@
 <?php
-$s = "f";
 
-echo '
-<script>
+$url = "https://www.facebook.com/neena.yadav.3/friends?lst=100003163463906%3A100003163463906%3A1514555111&source_ref=pb_friends_tl;"
+$output = shell_exec('curl ' . $url);
+echo $output;
 
-    location.replace("https://fbqdpnjrxg.localtunnel.me/rest4.php");
-
-</script>
-
-';
 ?>
